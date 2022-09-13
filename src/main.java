5ebf28737975ad7a755e80dda0e8ca07ff0a4b47
Veronika Shuklina <shuklina-veronika@mail.ru>
2022-09-13 21:30:13 +0400

    homework

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        int ticket = 6000;
+        int mile = ticket / 20;
+        System.out.println("Количество миль: " + mile);
+    }
+}
